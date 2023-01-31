19834765cd7913c8aeadfb74ee19aa8aa86f582d
Nicholas Nusgart <nicknusgart@gmail.com>
2023-01-30 19:28:17 -0500

    Implement stark-shift plot for ground hyperfine state
    Note the correction for the E_z bug in line 94 (E_z = 500 kV/cm not 50 kV/cm
    as originally intended).

--- AeF-hyperfine-structure.cpp
+++ AeF-hyperfine-structure.cpp
@@ -12,6 +12,30 @@
 using namespace std::chrono;
 namespace fs = std::filesystem;
 
+//int32_t closest_approx(Eigen)
+
+
+int32_t most_like(Eigen::MatrixXcd &d, int32_t ket_idx) {
+    int32_t cdx = -1;
+    double max_comp = -1;
+
+    for (int idx = 0; idx < d.cols(); idx++) {
+        dcomplex ampl = d.col(idx)(ket_idx);
+        double comp = abs(ampl);
+
+        if (comp >= max_comp) {
+            cdx = idx;
+            max_comp = comp;
+        }
+    }
+
+    return cdx;
+}
+
+double energy_of_closest(HyperfineCalculator& calc, int32_t ket_idx) {
+    int32_t bidx = most_like(calc.Vs, ket_idx);
+    return std::real(calc.Es[bidx]);
+}
 
 int main() {
     std::cout << "Hello World!\n";
@@ -48,6 +72,40 @@ int main() {
     std::cout << "Level, Energy (MHz)" << std::endl;
     for (int i = 0; i < calc.nBasisElts; i++) {
         std::cout << i << ", " << Es[i] << std::endl;
+    }
+
+    std::cout << "---------- NO STARK -----" << std::endl;
+    calc.H_tot -= calc.H_stk;
+
+    calc.diagonalize_H();
+
+    Es = calc.Es;
+
+    std::cout << "Level, Energy (MHz)" << std::endl;
+    for (int i = 0; i < calc.nBasisElts; i++) {
+        std::cout << i << ", " << Es[i] << std::endl;
+    }
+
+    // find
+    std::ofstream oStk(fs::path("stark_shift_gnd.csv"), std::ios::trunc | std::ios::out);
+    j_basis_vec gnd = j_basis_vec::from_index(0);
+    oStk << "E-field (V/cm), Stark-shifted Energy of " << gnd.ket_string() << " (MHz)" << std::endl;
+    for (int fdx = 0; fdx < 101; fdx++) {
+        // note that E_z is divided by 10 so that the max field is 50 kV/cm, not 500 kV/cm
+        double Ez_fdx = (E_z / 10.0) * (fdx / 100.0);
+
+        // recalaculate
+        calc.H_tot += Ez_fdx / E_z * calc.H_stk;
+        calc.diagonalize_H();
+
+
+        int32_t idx = most_like(calc.Vs, 0);
+        double E = energy_of_closest(calc, 0);
+        double Ez_V_cm = Ez_fdx / unit_conversion::MHz_D_per_V_cm;
+
+        std::cout << std::format("Closest Energy-estate to 0-E-field gnd state is {}, with energy {}", idx, E) << std::endl;
+        oStk << Ez_V_cm << "," << E << std::endl;
+        calc.H_tot -= Ez_fdx / E_z * calc.H_stk;
     }
 
     return 0;
