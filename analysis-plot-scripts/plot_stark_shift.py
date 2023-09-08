bbb7559909c3415cab6abd27f31d388c8c9ed6fa
Nicholas Nusgart <nicknusgart@gmail.com>
2023-09-08 17:27:41 -0400

    Make plot_stark_shift plot more similarly to the 2018 EDM3 PRA paper's figure 2
    Note that that figure has dimensions of 756x857

--- analysis-plot-scripts/plot_stark_shift.py
+++ analysis-plot-scripts/plot_stark_shift.py
@@ -14,9 +14,26 @@ import numba
 
 
 rundir = r'C:\Users\nusgart\source\AeF-hyperfine-structure\output\2023-07-19-181153.8494779'
-run = os.path.split(rundir)[1]
 if len(sys.argv) > 1:
     rundir = sys.argv[1]
+
+rundir = os.path.abspath(rundir)
+x_pix = 1366
+y_pix = 900
+
+if len(sys.argv) > 3:
+    x_pix = int(sys.argv[2])
+    y_pix = int(sys.argv[3])
+
+out_fname = "stark_shift_gnd.png"
+
+# default fname for pra aspect ratios
+if x_pix == 756:# and y_pix == 857:
+    out_fname = "stark_shift_gnd_pra_aspect.png"
+
+if len(sys.argv) > 4:
+    out_fname = sys.argv[4]
+run = os.path.split(rundir)[1]
 
 starkpath = os.path.join(rundir, 'stark_shift_gnd.csv')
 df = pd.read_csv(starkpath)
@@ -28,12 +45,25 @@ key_dEf1t = ' dE_f1t'
 key_dEf10 = ' dE_f10'
 key_dEf11 = ' dE_f11'
 
-fig = plt.figure(figsize=(13.66, 9.00))
+
+props = dict(boxstyle='round', facecolor='wheat', alpha=0.5)
+
+fig,ax = plt.subplots(2, 1, figsize=(x_pix / 100.0, y_pix / 100.0), sharex = True, sharey = False)
+## Overall shift
 gca = plt.subplot(2, 1, 1)
-gca.set_title(f'F=1 Ground-relative Stark shift for run {run}')
-df.plot(key_E, [key_dEf1t, key_dEf10, key_dEf11], ax=gca)
+gca.tick_params(axis='both', which='both', direction='inout')
+textstr = f'Ground State Stark Shift'
+gca.text(0.05, 0.1, textstr, transform=gca.transAxes, fontsize=14, verticalalignment='top', bbox=props)
+df.plot(key_E, [key_dEgnd,], ax=gca, ylabel='Energy (MHz)')
+# Ground-relative shift
 gca = plt.subplot(2, 1, 2)
-gca.set_title(f'Ground State Stark Shift run {run}')
-df.plot(key_E, key_dEgnd, ax=gca, ylabel='Energy (MHz)')
-plt.savefig(os.path.join(rundir, "stark_shift_gnd.png"))
+gca.tick_params(axis='both', which='both', direction='inout')
+textstr = f'F=1 Ground-relative Stark shift'
+gca.text(0.05, 0.1, textstr, transform=gca.transAxes, fontsize=14, verticalalignment='top', bbox=props)
+df.plot(key_E, [key_dEf1t, key_dEf10, key_dEf11], ylabel='Energy (MHz)', ax=gca)
+#
+fig.suptitle(f"N=0, F=0,1 Stark shift for run {run}", y=0.999)
+plt.subplots_adjust(bottom=0.05, right=0.998, top=0.97, left = 0.114, hspace = 0.0)
+# 
+plt.savefig(os.path.join(rundir, out_fname))
 plt.show()
