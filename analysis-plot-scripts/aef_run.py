31ece89299517ffa6d6fe14e7fd6b4628fbe84da
Nicholas Nusgart <nicknusgart@gmail.com>
2024-02-19 18:15:43 -0500

    Fix aef_run to work when supplied paths ending with a directory seperator

--- analysis-plot-scripts/aef_run.py
+++ analysis-plot-scripts/aef_run.py
@@ -44,9 +44,10 @@ def get_ssv_val(line, marker, typ=int):
 class aef_run(object):
     
     def __init__(self, dir_path):
-        self.path = dir_path
-        self.run = os.path.split(dir_path)[1]
+        self.path = dir_path.rstrip('/\\')
+        self.run = os.path.split(self.path)[1]
         ts = self.run.split('.')
+        print(self.run)
         us = ts[1][0:5]
         rs = ts[0] + '.' + us
         self.timestamp = datetime.datetime.strptime(rs, "%Y-%m-%d-%H%M%S.%f")
