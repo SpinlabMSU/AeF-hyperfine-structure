625176a5c4a43d2d262ffce8f3200a4584eddbdd
Nicholas Nusgart <nicknusgart@gmail.com>
2024-06-10 10:36:07 -0400

    Python script bugfix: properly distinguish calc_E_z and max_E_z
    This bug was introduced when calc_E_z was seperated out from max_E_z in commit a067f27

--- analysis-plot-scripts/aef_run.py
+++ analysis-plot-scripts/aef_run.py
@@ -59,12 +59,19 @@ class aef_run(object):
         self.dev_en = False
         self.dev_K = math.nan
         self.nmax = -1 # n is the orbitorotational quantum number
-        self.max_E_z = math.nan
+        self.max_E_z = -math.inf
+        self.calc_E_z = math.nan
 
         self.stk_lop_dur = -1
         self.mat_elt_dur = -1
 
         self.cdir = os.path.join(self.path, 'state_coeffs')
+        self.state_info_dir = os.path.join(self.path, 'state_info')
+
+        if not os.path.exists(self.state_info_dir):
+            ## old name was devonshire_info because this information was originally only computed when the
+            ## devonshire potential was enabled to check for orientation-locking
+            self.state_info_dir = os.path.join(self.path, 'devonshire_info')
 
         if self.valid:
             self.parse_log()
@@ -82,17 +89,18 @@ class aef_run(object):
             n_search = "nmax is"
             e_search = "E_z is" # externally applied
             k_search = "K is" # devonshire coupling constant named K, look for enabled
-            if "nmax is" in line and k_search in line:
+            Emax_search = "Electric field strength is"
+            if n_search in line and k_search in line:
                 print(f"Found parameter line \"{line}\"")
 
                 #ldx = line.find(n_search) + len(n_search)
                 self.nmax = get_ssv_val(line, n_search, int)#self.nmax = int(line[ldx:].split(' ')[0])
 
                 #ldx = line.find(e_search) + len(e_search)
-                self.max_E_z = get_ssv_val(line, e_search, float)#self.max_E_z = float(line[ldx:].split(' ')[0])
+                self.calc_E_z = get_ssv_val(line, e_search, float)#self.max_E_z = float(line[ldx:].split(' ')[0])
 
                 ldx = line.rfind(k_search) + len(k_search)
-                dev_K = float(line[ldx+1:].split(' ')[0])
+                self.dev_K = float(line[ldx+1:].split(' ')[0])
             
                 tdx = line.find('(', ldx) + 1
                 ena_text = 'enabled'
@@ -106,6 +114,10 @@ class aef_run(object):
                     print(text.lower())
                     raise RuntimeError("Parameter line doesn't specify whether devonshire was enabled!")
                 found_param_line = True
+            elif Emax_search in line:
+                # parse 
+                # note: the last 
+                self.max_E_z = max(get_ssv_val(line, Emax_search, float), self.max_E_z)
             elif "have taken" in line:
                 lline = line.lower()
                 print(f'found have taken line "{lline}"')
@@ -118,7 +130,9 @@ class aef_run(object):
                     print(f'found stark loop dur {dur}')
         f.close()
         if not found_param_line:
-            raise RuntimeError(f"Devonshire status not found in {self.log_path}")
+            raise RuntimeError(f"Parameter line not found in {self.log_path}")
+        if not math.isfinite(self.max_E_z):
+            raise RuntimeError(f"Unable to find maximum Electric field strength in {self.log_path}")
         return self
 
     def parse_gnd_stark_shift(self, *args, **kwargs):
@@ -146,7 +160,19 @@ class aef_run(object):
                 csvlist.append(ent.path)
         return csvlist
         
-        
+    def get_state_ifo_dir(self):
+        return self.state_info_dir
+
+    def get_state_ifo_Ez(self, Ez, *args, **kwargs):
+        csvpath = os.path.join(self.get_state_ifo_dir(), f'info_Ez_{E_z}.csv')
+        return pd.read_csv(csvpath, *args, **kwargs)
+
+    def list_ifo_csvs(self):
+        csvlist = []
+        for ent in os.scandir(self.get_state_ifo_dir()):
+            if ent.is_file() and ent.name.lower().endswith('.csv'):
+                csvlist.append(ent.path)
+        return csvlist
 
 def find_runs(scandir):
     runlist = []
