d2f9a91421965fa0337237fb9eefeab31b695fbf
Nicholas Nusgart <nicknusgart@gmail.com>
2025-08-01 14:41:23 -0400

    Update RaF prod coefficients with predictions from Phys. Rev. A 102, 062801

--- SpinlabHyperfineLib/include/aef/raf_constants.h
+++ SpinlabHyperfineLib/include/aef/raf_constants.h
@@ -1,5 +1,5 @@
 /*
-  aef/raf_constants.hpp -- constants related to the rotohyperfine structure of 225RaF
+  aef/raf_constants.hpp -- constants related to the rohyperfine structure of 225RaF
   Note: as a pure collection of constants, this file is not eligible to be copyrighted
   in the United States.  For any place/time where this is held to not be the case, 
   I place this file into the public domain following the CC0.  See 
@@ -9,7 +9,51 @@
 #define _RAF_CONSTANTS_HPP 1
 #pragma once
 #include <aef/units.hpp>
+
 namespace aef::raf_constants {
+    ///// sourced from [0] PRA 98, 032513 (2018) --> these are for BaF not RaF
+    //// and from [1] Nat Phys 20, 202-207 (2024)
+    //// and from [2] https://arxiv.org/abs/2311.04121
+    //// and from [3] Phys. Rev. A 102, 062801
+    //// and from [2] https://arxiv.org/pdf/1302.5682.pdf??
+
+    // constants for rotational hamiltonian, taken from 
+    constexpr double B = 0.191985 * unit_conversion::MHz_per_inv_cm; // MHz, from [1]
+    constexpr double D = 1.40E-7 * unit_conversion::MHz_per_inv_cm; // MHz, from [1]
+    constexpr double gamma = 0.00585 * unit_conversion::MHz_per_inv_cm; // MHz, from [1]
+    constexpr double delta = 0.111 * 1e-3; // kHz --> MHz -- not measured yet, using 138BaF value from [0]
+
+    /// constants for Radium-225 hyperfine shift in 225RaF
+    constexpr double A_para_Ra = -0.5692 * unit_conversion::MHz_per_inv_cm; // MHz, from [2]
+    constexpr double A_perp_Ra = -0.5445 * unit_conversion::MHz_per_inv_cm; // MHz, from [2]
+    constexpr double b_Ra = A_perp_Ra; // MHz --> used in fermi contact
+    constexpr double c_Ra = A_para_Ra - A_perp_Ra;  // MHz --> usid in fermi contact and dipolar spin-spin
+    constexpr double c_I_Ra = 0.00; // MHz --> used in nuclear spin-rotation, not measured
+    // no electric quadrupole because I(225Ra) = 1/2
+
+    /// constants for fluorine-19 hyperfine shift in 225RaF -- not currently measured,
+    /// using values predicted by Phys. Rev. A 102, 062801
+    constexpr double A_para_F = 109; // MHz, from [3]
+    constexpr double A_perp_F = 90; // MHz, from [3]
+    
+    constexpr double b_F = A_perp_F; // MHz --> used in fermi contact
+    constexpr double c_F = A_para_F - A_perp_F;  // MHz --> usid in fermi contact and dipolar spin-spin
+    constexpr double c_I_F = 0.00; // MHz --> used in nuclear spin-rotation
+
+    // no electric quadrupole because I(19F) = 1/2
+    // I guess no I1 \cdot I2 term???
+
+    // constants for stark shift
+    constexpr double mu_e = 3.170; // D, from [0] for now (BaF number, not RaF)
+
+
+    // explicit rotational symmetry breaking term to break m_f degeneracy
+    // no longer needed
+    constexpr double e_mf_break = 0;// 1.0E-6; // MHz
+};
+
+// used before 2025-08-01
+namespace aef::raf_constants::old {
     ///// sourced from PRA 98, 032513 (2018) --> these are for BaF not RaF
     //// and from ??https://arxiv.org/pdf/1302.5682.pdf??
     //// and from Nat Phys 20, 202-207 (2024)
@@ -46,7 +90,6 @@ namespace aef::raf_constants {
     // no longer needed
     constexpr double e_mf_break = 0;// 1.0E-6; // MHz
 };
-
 
 /// <summary>
 /// This set of constants is for testing purposes only and is identical to the constants for $^{138}$BaF
