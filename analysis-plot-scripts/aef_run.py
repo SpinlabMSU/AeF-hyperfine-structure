a1fb4505ab67d2268ca8511d59fa5e410c9af1bb
Nicholas Nusgart <nicknusgart@gmail.com>
2026-03-02 16:22:21 -0500

    Take absolute path of runpath in aef_run.py

--- analysis-plot-scripts/aef_run.py
+++ analysis-plot-scripts/aef_run.py
@@ -44,6 +44,7 @@ def get_ssv_val(line, marker, typ=int):
 class aef_run(object):
     
     def __init__(self, dir_path):
+        dir_path = os.path.abspath(dir_path)
         dir_path = dir_path.rstrip('/\\')
         self.path = dir_path
         self.run = os.path.split(dir_path)[1]
